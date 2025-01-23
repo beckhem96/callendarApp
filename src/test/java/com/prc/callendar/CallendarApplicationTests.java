f3337eaaec75623e7817790a34036cbd904f646b
beckhem96 <beckhem96@naver.com>
2025-01-23 23:19:29 +0900

    init files

---
+++ src/test/java/com/prc/callendar/CallendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prc.callendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
